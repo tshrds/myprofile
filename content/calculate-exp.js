58df97745f21de3f620dfd0c00d15a1be6599528
Tushar Das <tdas@calibo.com>
2023-10-13 13:43:55 +0530

    changes to exp

--- content/calculate-exp.js
+++
@@ -1,7 +0,0 @@
-function totalExperience() {
-    const date2 = new Date('2015-07-13');
-    const monthDiff = new Date().getMonth() - date2.getMonth();
-    const yearDiff = new Date().getYear() - date2.getYear();
-
-    return yearDiff + '.' + monthDiff;
-}
